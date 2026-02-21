d6b1173796c05514231ce31ed7ba85de09ae5e1c
shikhar004dot-com <shikharsrivastava816@gmail.com>
2026-02-21 15:01:37 +0530

    fix static routinh

--- server/server.js
+++ server/server.js
@@ -1,35 +1,31 @@
 require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
-const dbConnect = require("./config/db")
+const express=require("express")
+const cors=require("cors")
+const path=require("path")
+const dbConnect=require("./config/db")
 
-const authRoutes = require("./routes/authRoutes")
-const libraryRoutes = require("./routes/libraryRoutes")
+const authRoutes=require("./routes/authRoutes")
+const libraryRoutes=require("./routes/libraryRoutes")
+const app=express()
 
-const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Server is running")
+app.use(express.static(path.join(__dirname,"../public")))
+
+app.use("/",authRoutes)
+app.use("/",libraryRoutes)
+
+const startServer=async()=>{
+try{
+await dbConnect()
+const PORT=process.env.PORT||8000
+app.listen(PORT,()=>{
+console.log(`Server running on port ${PORT}`)
 })
-
-app.use("/", authRoutes)
-app.use("/", libraryRoutes)
-
-app.use(express.static(path.join(__dirname, "../public")))
-
-const startServer = async () => {
-    try {
-        await dbConnect()
-        const PORT = process.env.PORT || 8000
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`)
-        })
-    } catch (err) {
-        console.log("Failed to connect DB", err)
-    }
+}catch(err){
+console.log("Failed to connect DB",err)
+}
 }
 
 startServer()
