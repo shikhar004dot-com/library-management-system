b7a61792cb69a64afbe31ed1e92c1b51204a381d
shikhar004dot-com <shikharsrivastava816@gmail.com>
2026-03-23 19:35:15 +0530

    fixed cors error

--- server/server.js
+++ server/server.js
@@ -1,31 +1,34 @@
-require("dotenv").config()
-const express=require("express")
-const cors=require("cors")
-const path=require("path")
-const dbConnect=require("./config/db")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-const authRoutes=require("./routes/authRoutes")
-const libraryRoutes=require("./routes/libraryRoutes")
-const app=express()
+const dbConnect = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const libraryRoutes = require("./routes/libraryRoutes");
+const app = express(); 
+app.use(cors({
+  origin: "https://library-management-ssystem.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
 
-app.use(cors())
-app.use(express.json())
+app.options("*", cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use(express.static(path.join(__dirname,"../public")))
-
-app.use("/",authRoutes)
-app.use("/",libraryRoutes)
+app.use("/", authRoutes);
+app.use("/", libraryRoutes);
 
 const startServer=async()=>{
-try{
-await dbConnect()
-const PORT=process.env.PORT||8000
-app.listen(PORT,()=>{
-console.log(`Server running on port ${PORT}`)
-})
-}catch(err){
-console.log("Failed to connect DB",err)
-}
-}
-
-startServer()
+  try{
+    await dbConnect();
+    const PORT=process.env.PORT || 8000;
+    app.listen(PORT,()=>{
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch(err){
+    console.log("Failed to connect DB", err);
+  }
+};
+startServer();
